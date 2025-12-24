29200da5a22d2e4a12bd0a8e89040c70a6368ab1
Dregi56 <79653938+Dregi56@users.noreply.github.com>
2025-12-24 22:32:07 +0100

    Update run.sh

--- alexa_audio_bridge/run.sh
+++ alexa_audio_bridge/run.sh
@@ -1,1 +1,15 @@
+#!/usr/bin/with-conten-env bashio
 
+bashio::log.info "Avvio di Alexa Audio Bridge (LMS)..."
+
+# Definiamo dove si trovano i file musicali in Home Assistant
+MUSIC_DIR="/media"
+
+# Avviamo il processo di Logitech Media Server
+# Usiamo i percorsi standard dell'immagine lmscommunity
+exec /usr/sbin/squeezeboxserver \
+  --cachedir /config/cache \
+  --prefsdir /config/prefs \
+  --logdir /config/logs \
+  --mediadir $MUSIC_DIR \
+  --noexit
